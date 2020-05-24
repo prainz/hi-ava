561a4680081481abd850cc3a74485c86f1210b35
prainz <65723896+prainz@users.noreply.github.com>
2020-05-24 15:45:34 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "Prain", quotes: "Whatever happens, keep breathing" },
+    { author: "Prain", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "Prain", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "Prain", quotes: "I love you." },
+    { author: "Prain", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "Prain", quotes: "I love you." },
+    { author: "Prain", quotes: "I'm sorry for loving you." },
 ];

69ac6e9ca79e4c0b470ce1a20d15defe697ab07c
prainz <65723896+prainz@users.noreply.github.com>
2020-05-24 15:28:49 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,7 @@
         class="vue-typed"
         :showCursor="false"
         :loop="true"
-        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'I always wanted to tell you... ^2500', 'Nevermind! >///<']"
+        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'Saya selalu ingin memberi tahu Kamu... ^2500', 'Sudahlah! >///<']"
         :backDelay="500"
         :typeSpeed="100"
         :backSpeed="20"
